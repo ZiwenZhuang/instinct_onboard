4f0f6bed3977da815de2fe0f9ea92f357b75e7e2
Ziwen Zhuang <leozhuangzw@outlook.com>
2024-12-19 22:11:54 +0800

    [add] automatic sync time code

--- g1_ethernet.sh
+++ g1_ethernet.sh
@@ -2,3 +2,5 @@
 source ./instinct-venv/bin/activate
 source ~/Projects/unitree_ros2/setup.sh
 source ~/Codes/ros2_numpy_ws/install/setup.sh
+echo "Syncing Time to Jetson"
+sshpass -p 123 ssh g1 "(echo 123 | sudo -S timedatectl set-timezone Asia/Shanghai) && (echo 123 | sudo -S date --set=\"$(date '+%Y-%m-%d %H:%M:%S')\")"
