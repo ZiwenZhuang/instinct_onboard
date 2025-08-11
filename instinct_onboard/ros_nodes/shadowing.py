f03ec07869ccb2e4daeda2d8ec4afd0583b9240d
Ziwen Zhuang <leozhuangzw@outlook.com>
2025-08-11 15:54:58 +0800

    [fix] shadowing node Point handling

--- instinct_onboard/ros_nodes/shadowing.py
+++ instinct_onboard/ros_nodes/shadowing.py
@@ -10,7 +10,7 @@ from .ros_real import Ros2Real
 
 
 class ShadowingNodeMixin:
-    def __init__(self, *args, motion_sequence_topic: str = "/motion_sequence", **kwargs):
+    def __init__(self, *args, motion_sequence_topic: str = "/motion_target", **kwargs):
         """Initialize the ShadowingNodeMixin with the motion sequence topic."""
         super().__init__(*args, **kwargs)
         self.motion_sequence_topic = motion_sequence_topic
@@ -49,7 +49,9 @@ class ShadowingNodeMixin:
         link_quat_mask = np.ones((num_frames, num_links), dtype=np.float32)  # (num_frames, num_links, 4)
         for i, frame in enumerate(msg.data):
             time_to_target[i] = frame.time_to_target
-            root_pos_b[i] = frame.root_pos_b
+            root_pos_b[i, 0] = frame.pos_b.x
+            root_pos_b[i, 1] = frame.pos_b.y
+            root_pos_b[i, 2] = frame.pos_b.z
             root_quat_w[i, 0] = frame.quat_w.x
             root_quat_w[i, 1] = frame.quat_w.y
             root_quat_w[i, 2] = frame.quat_w.z
@@ -58,7 +60,9 @@ class ShadowingNodeMixin:
                 joint_pos[i, j] = frame.joint_pos[j]  # in urdf space, simulation order.
                 joint_pos_mask[i, j] = frame.joint_pos_mask[j]  # (num_frames, num_joints)
             for j in range(num_links):
-                link_pos[i, j] = frame.link_pos[j]
+                link_pos[i, j, 0] = frame.link_pos[j].x
+                link_pos[i, j, 1] = frame.link_pos[j].y
+                link_pos[i, j, 2] = frame.link_pos[j].z
                 link_quat[i, j, 0] = frame.link_quat[j].w
                 link_quat[i, j, 1] = frame.link_quat[j].x
                 link_quat[i, j, 2] = frame.link_quat[j].y
